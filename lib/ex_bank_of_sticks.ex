319e705a851151ab0f20e324804085ff4e3a637f
Edwin Mak <cintosyntax@gmail.com>
2018-11-07 07:21:55 -0500

    Init commit

---
+++ lib/ex_bank_of_sticks.ex
@@ -0,0 +1,18 @@
+defmodule ExBankOfSticks do
+  @moduledoc """
+  Documentation for ExBankOfSticks.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExBankOfSticks.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_bank_of_sticks_test.exs
@@ -0,0 +1,8 @@
+defmodule ExBankOfSticksTest do
+  use ExUnit.Case
+  doctest ExBankOfSticks
+
+  test "greets the world" do
+    assert ExBankOfSticks.hello() == :world
+  end
+end
